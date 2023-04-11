275f9021109ac0e32e0b60aaafb459df1f3acb78
stnMboa237 <stngassa@live.com>
2023-04-11 11:27:59 +0200

    sect22-securty Headers Added

--- API/Program.cs
+++ API/Program.cs
@@ -30,11 +30,54 @@ plus bas soient gerées par le middlware
 */
 app.UseMiddleware<ExceptionMiddleware>();
 
+/* start: config the http Header for security policies(directly after ExecptionMiddleware)*/
+
+app.UseXContentTypeOptions(); //header = X-Content-Type-Options: prevents app from Mime Sniffing pof the content type
+app.UseReferrerPolicy(opt => opt.NoReferrer()); //header = Referrer-Policy
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode()); // header = Content-Security-Policy: add a protection against the cross site scripting header
+app.UseXfo(opt => opt.Deny()); //X-Frame-Options
+
+// we use this in dev Mode to check the headers which we want to enable via the "CustomSource(...)"
+// app.UseCspReportOnly(opt => opt //header= Content-Security-Policy-Report-Only
+//     .BlockAllMixedContent() //ceci force l'app á charger que du contenu HTTPS ou juste HTTP. Mais pas un mix des 2
+//     .StyleSources(s => s.Self()
+//         .CustomSources("https://fonts.googleapis.com") // exception: ok pour ce contenu
+//     ) //approuve seulement les CSS provenant de notre APP (wwwroot)
+//     .FontSources(s => s.Self()
+//         .CustomSources("https://fonts.gstatic.com", "data:") // exception: ok pour ce contenu
+//     ) //approuve seulement les fonts provenant de notre APP (wwwroot)
+//     .FormActions(s => s.Self()) //approuve seulement les form action provenant de notre APP (wwwroot)
+//     .FrameAncestors(s => s.Self()) //approuve seulement les frame provenant de notre APP (wwwroot)
+//     .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com")) //approuve seulement les images provenant de notre APP (wwwroot)
+//     .ScriptSources(s => s.Self()) //approuve seulement les scripts action provenant de notre APP (wwwroot)
+// );
+
+app.UseCsp(opt => opt //header= Content-Security-Policy-Report-Only
+    .BlockAllMixedContent() //ceci force l'app á charger que du contenu HTTPS ou juste HTTP. Mais pas un mix des 2
+    .StyleSources(s => s.Self()
+        .CustomSources("https://fonts.googleapis.com") // exception: ok pour ce contenu
+    ) //approuve seulement les CSS provenant de notre APP (wwwroot)
+    .FontSources(s => s.Self()
+        .CustomSources("https://fonts.gstatic.com", "data:") // exception: ok pour ce contenu
+    ) //approuve seulement les fonts provenant de notre APP (wwwroot)
+    .FormActions(s => s.Self()) //approuve seulement les form action provenant de notre APP (wwwroot)
+    .FrameAncestors(s => s.Self()) //approuve seulement les frame provenant de notre APP (wwwroot)
+    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com")) //approuve seulement les images provenant de notre APP (wwwroot)
+    .ScriptSources(s => s.Self()) //approuve seulement les scripts action provenant de notre APP (wwwroot)
+);
+
+/* end: config the http Header for security policies(directly after ExecptionMiddleware)*/
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}else {
+    app.Use(async (context, next) => {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 //attention á la disposition des middleware: 
